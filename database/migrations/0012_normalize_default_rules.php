bd2f6f491bd0dcf8b0a364649d47bb7cfd705ebd
mszaitsev <mszaitsev@gmail.com>
2026-05-24 23:05:56 +0700

    fix rules db

--- database/migrations/0012_normalize_default_rules.php
+++ database/migrations/0012_normalize_default_rules.php
@@ -8,5 +8,14 @@ return static function (): void {
 
 	$table_name = $wpdb->prefix . 'wdc_rules';
 
+	$table_exists = static function ( string $table ) use ( $wpdb ): bool {
+		$result = $wpdb->get_var( $wpdb->prepare( 'SHOW TABLES LIKE %s', $table ) );
+		return ! in_array( $result, array( null, '', 0, '0' ), true );
+	};
+
+	if ( ! $table_exists( $table_name ) ) {
+		return;
+	}
+
 	$wpdb->query( "UPDATE {$table_name} SET target_type = 'default', target_value = '' WHERE target_type IS NULL OR target_type = ''" );
 };
